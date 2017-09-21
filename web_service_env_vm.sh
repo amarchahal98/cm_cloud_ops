0cdad13aef2421eb2cdd37cff24b17bfd670477c
Amar Chahal <achahal@sw08.lab471.htpbcit.ca>
2017-09-20 22:19:51 -0700

    test

--- web_service_env_vm.sh
+++ web_service_env_vm.sh
@@ -30,14 +30,12 @@ VBoxManage createhd --filename ~/VirtualBox\ VMs/$name/$name.vdi --size 20480
 VBoxManage storageattach $name --storagectl "IDE Controller" --port 0 --device 1 --type hdd --medium ~/VirtualBox\ VMs/$name/$name.vdi > /dev/null 2>&1 
 VBoxManage storageattach $name --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium /usr/share/virtualbox/VBoxGuestAdditions.iso # Attach the VirtualBox Guest Additions CD
 
-# Copies files to the PXE Server. SSH Public key has been added on the remote server to circumvent password prompt
-scp -P 60022 files/wp_ks.cfg root@127.0.0.1:/usr/share/nginx/html/
-scp -P 60022 files/www.conf root@127.0.0.1:/usr/share/nginx/html/
-scp -P 60022 files/nginx.conf root@127.0.0.1:/usr/share/nginx/html/
-scp -P 60022 files/wp-config.php root@127.0.0.1:/usr/share/nginx/html/
-scp -P 60022 files/wp_mariadb_config.service root@127.0.0.1:/usr/share/nginx/html/
-scp -P 60022 files/php.ini root@127.0.0.1:/usr/share/nginx/html/
-scp -P 60022 files/mariadb_security_config.sql root@127.0.0.1:/usr/share/nginx/html/
-scp -P 60022 files/wp_mariadb_config.sh root@127.0.0.1:/usr/share/nginx/html/
 
 vboxmanage startvm $name
+
+until [[ $(ssh -q wp exit && echo "online") == "online" ]] ; do
+  sleep 10s
+  echo "waiting for wp to come online"
+done
+
+ansible-playbook -v -i hosts site.yml
