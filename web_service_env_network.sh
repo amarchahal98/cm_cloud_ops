66c9c1adf1cf006caa5d98eb0453ad2bdd64bd9b
Amar Chahal <achahal@sw08.lab471.htpbcit.ca>
2017-09-21 18:33:24 -0700

    test passed

--- web_service_env_network.sh
+++ web_service_env_network.sh
@@ -4,14 +4,18 @@ name="wordpress"
 nat_name="nasp_cm_co"
 
 # Host Firewall Configuration
-for var in 50022 50080 50443; do sudo firewall-cmd --zone=public --add-port=${var}/tcp --permanent; done
-
+for var in 50022 50080 50443 60022; do sudo firewall-cmd --zone=public --add-port=${var}/tcp --permanent; done
+sudo systemctl restart firewalld
 # Virtual Box Network Configuration
 vboxmanage natnetwork add --netname "${nat_name}" --network "192.168.254.0/24" --dhcp off
 
 
-#IPV4 Port Forwards
-vboxmanage natnetwork modify --netname $nat_name --port-forward-4 "ssh:tcp:[]:50022:[192.168.254.10]:22"
-vboxmanage natnetwork modify --netname $nat_name --port-forward-4 "http:tcp:[]:50080:[192.168.254.10]:80"
-vboxmanage natnetwork modify --netname $nat_name --port-forward-4 "https:tcp:[]:50443:[192.168.254.10]:443"
+# IPV4 Port Forwards for wordpress
+vboxmanage natnetwork modify --netname nasp_cm_co --port-forward-4 "ssh:tcp:[]:50022:[192.168.254.10]:22"
+vboxmanage natnetwork modify --netname nasp_cm_co --port-forward-4 "http:tcp:[]:50080:[192.168.254.10]:80"
+vboxmanage natnetwork modify --netname nasp_cm_co --port-forward-4 "https:tcp:[]:50443:[192.168.254.10]:443"
+
+# IPV4 Port Forwards for pxe
+vboxmanage natnetwork modify --netname nasp_cm_co --port-forward-4 "pxessh:tcp:[]:60022:[192.168.254.5]:22"
+
 
